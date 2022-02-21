ff13a8575f14eaf70de8fc4aa7c794ec0b53d671
jongwoo <leejongwoo.dev@gmail.com>
2022-02-21 21:54:25 +0900

    Create Spring Boot Project

---
+++ src/main/java/nice/jongwoo/SpringReactCalendarTodoApplication.java
@@ -0,0 +1,13 @@
+package nice.jongwoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactCalendarTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReactCalendarTodoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nice/jongwoo/SpringReactCalendarTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package nice.jongwoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactCalendarTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
