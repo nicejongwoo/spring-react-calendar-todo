769fb630fb2d39b7c47ddb4c42fe647e5cb33129
jongwoo <leejongwoo.dev@gmail.com>
2022-02-21 22:00:20 +0900

    editorconfig추가

--- src/main/java/nice/jongwoo/SpringReactCalendarTodoApplication.java
+++ src/main/java/nice/jongwoo/SpringReactCalendarTodoApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringReactCalendarTodoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringReactCalendarTodoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringReactCalendarTodoApplication.class, args);
+    }
 
 }
