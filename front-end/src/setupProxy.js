d1ef85fc3d31d4f7bf170db2826a6ab58de014c1
leejongwoo <leejongwoo.dev@gmail.com>
2022-04-11 22:31:17 +0900

    Fix proxy

--- front-end/src/setupProxy.js
+++ front-end/src/setupProxy.js
@@ -1,6 +1,6 @@
-const proxy = require('http-proxy-middleware')
+// const proxy = require('http-proxy-middleware')
 
-module.exports = function(app) {
-    app.use(proxy('/*'), {target: 'http://localhost:8081'})
-    app.use(proxy('/*'), {target: 'http://localhost:8082'})
-}
+// module.exports = function(app) {
+//     app.use(proxy('/*'), {target: 'http://localhost:8080'})
+//     app.use(proxy('/*'), {target: 'http://localhost:8082'})
+// }
